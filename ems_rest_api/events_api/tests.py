c715a14abab8a843645e62589b618bcdd240d7b8
Dzhaner Hasan <dzhanerrhasan@gmail.com>
2023-10-04 13:07:50 +0300

    finshing touches

--- ems_rest_api/events_api/tests.py
+++ ems_rest_api/events_api/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Event
 
-# Create your tests here.
+
+class EventModelTest(TestCase):
+    def setUp(self):
+        self.event = Event.objects.create(
+            title="Test Event",
+            location="Test Location",
+            date="2023-01-01",
+            time="10:00:00",
+            description="Test Description",
+        )
+
+    def test_title_label(self):
+        event = self.event
+        field_label = event._meta.get_field("title").verbose_name
+        self.assertEqual(field_label, "title")
+
+    def test_location_label(self):
+        event = self.event
+        field_label = event._meta.get_field("location").verbose_name
+        self.assertEqual(field_label, "location")
+
+    def tearDown(self):
+        self.event.delete()
